fc4c499dbadae7620b6cd2066627625c15250ef7
duongdx-kma <duong.dinh.xuan998@gmail.com>
2024-09-21 14:56:20 +0700

    edit java - for maven test profile

--- src/test/java/io/john/programming/todoapp/TodoAppApplicationTests.java
+++ src/test/java/io/john/programming/todoapp/TodoAppApplicationTests.java
@@ -2,12 +2,13 @@ package io.john.programming.todoapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TodoAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
